a83c007e39fb188e23a118ab3692c484db016513
Sachin Patel <sachin5patel0@gmail.com>
2024-09-27 21:03:01 +0530

    Uploaded Procfile

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def predict_api():
     return render_template("home.html", prediction_text="Predicted House Price: {}".format(output[0]))
 
 
+
 if __name__=="__main__":
 #	app.run(debug=True)
 	app.run(host='0.0.0.0', port=5000, debug=True)
